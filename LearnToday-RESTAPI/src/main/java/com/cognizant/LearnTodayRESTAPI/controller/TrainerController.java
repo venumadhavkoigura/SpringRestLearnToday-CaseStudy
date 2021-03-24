8916fb795f3e09ac454aed30823b7ad644219015
venumadhavkoigura <venumadhavkoigura@gmail.com>
2021-03-24 22:01:38 +0530

    first commit Venumadhav

---
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/LearnTodayRESTAPI/controller/TrainerController.java
@@ -0,0 +1,44 @@
+package com.cognizant.LearnTodayRESTAPI.controller;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.LearnTodayRESTAPI.dao.TrainerDao;
+import com.cognizant.LearnTodayRESTAPI.dao.exception.InvalidTrainerIdException;
+import com.cognizant.LearnTodayRESTAPI.dao.impl.TrainerDaoImpl;
+import com.cognizant.LearnTodayRESTAPI.model.Trainer;
+
+@RestController
+@RequestMapping("/api/trainer")
+public class TrainerController {
+
+	@PostMapping("/trainerSignUp")
+	public ResponseEntity<Object> trainerSignUp(@RequestBody Trainer trainer){
+		TrainerDao dao = new TrainerDaoImpl();
+		try {
+			dao.signUp(trainer);
+			return new ResponseEntity<>(trainer, HttpStatus.CREATED);
+		} catch (DuplicateKeyException e) {
+			return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
+		}
+	}       
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updatePassword(@PathVariable int id, @RequestBody Trainer trainer){
+		TrainerDao dao = new TrainerDaoImpl();
+			try {
+				dao.updatePassWord(id, trainer);
+				return new ResponseEntity<>("Data updated successfully", HttpStatus.OK);
+			} catch (InvalidTrainerIdException e) {
+				e.printStackTrace();
+				return new ResponseEntity<>("Searched data not found", HttpStatus.NOT_FOUND);
+			}
+	}
+}
---
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/LearnTodayRESTAPI/model/Student.java
@@ -0,0 +1,28 @@
+package com.cognizant.LearnTodayRESTAPI.model;
+
+public class Student {
+	private Integer enrollmentId;
+	private Integer studentId;
+	private Integer courseId;
+	public Integer getEnrollmentId() {
+		return enrollmentId;
+	}
+	public void setEnrollmentId(Integer enrollmentId) {
+		this.enrollmentId = enrollmentId;
+	}
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public Integer getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(Integer courseId) {
+		this.courseId = courseId;
+	}
+	
+	
+	
+}
